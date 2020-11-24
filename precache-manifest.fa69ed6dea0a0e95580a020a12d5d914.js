4b5b1d89d095af8ab6f8b86c2a14633802cf3df5
Lydie Cherilus <lydiecherilus@yahoo.fr>
2020-11-24 16:28:01 -0500

    Updates

---
+++ precache-manifest.fa69ed6dea0a0e95580a020a12d5d914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bf74d76eaca8499a5fffbf9805ba92",
+    "url": "/nasa-photo-of-the-day/index.html"
+  },
+  {
+    "revision": "89ca7e10ba9f54de6bb5",
+    "url": "/nasa-photo-of-the-day/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "07595c9c65a40753a3ba",
+    "url": "/nasa-photo-of-the-day/static/css/main.847ecae6.chunk.css"
+  },
+  {
+    "revision": "89ca7e10ba9f54de6bb5",
+    "url": "/nasa-photo-of-the-day/static/js/2.b31265c0.chunk.js"
+  },
+  {
+    "revision": "07595c9c65a40753a3ba",
+    "url": "/nasa-photo-of-the-day/static/js/main.fe9806fe.chunk.js"
+  },
+  {
+    "revision": "350a0b8fbf7e20aef48a",
+    "url": "/nasa-photo-of-the-day/static/js/runtime~main.03586219.js"
+  }
+]);
